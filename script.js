f44b447082d1d914b1540d0c1f20c498f3cc1748
Edgaras <e.jurgelevicius@gmail.com>
2023-12-12 19:29:21 +0200

    correct

--- script.js
+++ script.js
@@ -10,15 +10,19 @@ function randomBoxSize(min, max) {
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1) + min)
 }
+const boxes = []
 
 const handleButton = () => {
   const container = document.querySelector('.container')
   const box = document.createElement('div')
+  const boxColor = color()
   container.style.visibility = 'visible'
   box.classList.add('box')
-  box.style.backgroundColor = color()
+  box.style.backgroundColor = boxColor
   const randomSize = randomBoxSize(10, 150) + 'px'
   box.style.width = randomSize
   box.style.height = randomSize
   container.appendChild(box)
+  boxes.push({ element: box, dimensions: randomSize, color: boxColor })
+  console.log(boxes)
 }
